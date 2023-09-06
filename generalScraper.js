a4759d4c3afcc23e7b8669b709fb750d178cf9ed
robinkbr <108469512+robinkbr@users.noreply.github.com>
2023-09-06 16:48:18 +0200

    Minor code change

--- generalScraper.js
+++ generalScraper.js
@@ -8,24 +8,27 @@ const openAI = new openai({
 });
 
 async function main() {
-    const websiteData = await cf.readCSV('./websiteData.csv');
+    const websiteData = await cf.readCSV('./websiteDataTest.csv');
     const page = await cf.initiate();
+    
     
     for (const websiteDatum of websiteData) {
         cf.prepareCSV(websiteDatum.fileName);
         await page.goto(websiteDatum.url);
 
-        let pageLinks;
         if (websiteDatum.pageVariant === 'pagination') {
+            let pageLinks;
             pageLinks = await cf.getLinksFromSelector(page, websiteDatum.pageSelector, websiteDatum.url);
 
             for (const pageLink of pageLinks) {
                 const linksToOpenCalls = await cf.getLinksFromSelector(page, websiteDatum.callSelector, websiteDatum.url);
                 await cf.extractData(page, websiteDatum.fileName, linksToOpenCalls, websiteDatum.callContentSelector, openAI);
+                
                 await page.goto(pageLink);
             }
         } else if (websiteDatum.pageVariant === 'showMore') {
             await cf.clickButtonWhileVisible(page, websiteDatum.buttonSelector);
+
             const linksToOpenCalls = await cf.getLinksFromSelector(page, websiteDatum.callSelector, websiteDatum.url);
             await cf.extractData(page, websiteDatum.fileName, linksToOpenCalls, websiteDatum.callContentSelector, openAI);
         } else {
